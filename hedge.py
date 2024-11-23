f20063b9c3032a32877b104a7c208298d7333a68
Janusz Grabis <janusz@frameworks.pl>
2024-11-23 06:35:56 +0100

    initial version

---
+++ hedge.py
@@ -0,0 +1,12 @@
+import logging
+
+class Hedge:
+    def __init__(self, repoRootPath):
+        self.repoRootPath = repoRootPath
+
+    def preflight():
+        agent.runCommand('apt-get update')
+
+
+    def vim():
+        agent.ensurePackages['neovim']
