e027aa9cd29236d454148afc52b0be8970df4f40
Janusz Grabis <janusz@framewroks.pl>
2025-04-01 11:16:15 +0200

    Preparing to extract target and then run it

--- k8s/fwk8s.py
+++ k8s/fwk8s.py
@@ -1,10 +1,14 @@
 import argparse
 
+def startPodSession():
+    pass
+
 def main():
 
+    
     parser = argparse.ArgumentParser(prog="k8s helper",
         description='Script to speed up operations on k8s')
-    # parser.add_argument('-r', "--repository", type=str, help='URL of the repository with server configuration')
+    #parser.add_argument('-it', "--interactive", type=str, help='Enter interactive session of a pod')
     # parser.add_argument('-m', "--module", type=str, help='Execute target from the provided module rather than from default one (hedge)', default='hedge')
     # parser.add_argument('-p', "--port", type=str, help='Port of the repository with server configuration', default=None)
     # parser.add_argument('-w', "--workdir", type=str, help='Location of work directory', default=None)
@@ -15,6 +19,13 @@ def main():
     # parser.add_argument('-o', "--sshoptions", type=str, help='SSH options', default="")
     
     args = parser.parse_args()
+
+    #TODO:
+    # - add target param, read it here
+    # - use value of targetr param to call appropriate target
+    # - inside target use -n param to get name of pod to run
+    # - write function that can figure out which 'real' pod to execute based on the name
+    targetMethod = getattr(hedgeInstance, target);
 
 
 if __name__ == '__main__':
