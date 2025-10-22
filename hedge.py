ff7b7d2c9016f172746fbf53db1f36957c792fcf
Janusz Grabis <janusz@frameworks.pl>
2025-10-22 23:31:37 +0200

    nvim target

--- hedge.py
+++ hedge.py
@@ -10,14 +10,27 @@ class Hedge:
         agent.runCommand('apt-get update')
 
     def vim(self, agent, params):
+
+        #NOTE: This method has limitations, for example it will
+        #      only update to certain version preventing lazyvim from working
+        #      use target neovim to get neovim installed manually to higher version
+
         if not os.environ['HOME']:
             logging.error('HOME environment variable is not define, cannot continue')
             return
         home = os.environ['HOME']
         agent.ensurePackages(['neovim'])
-
         
         agent.ensureFile('/vim/.vimrc', f"{home}/.vimrc")
+
+    def nvim(self, agent, params):
+        tmp = agent.getTempPath()
+        agent.runCommand(f"wget https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.tar.gz -o {tmp}/nvim-linux-x86_64.tar.gz")
+        logging.info(f"Extracted to {tmp}/nvim-linux-x86_64.tar.gz")
+
+
+    def lazyvim(self, agent, params):
+        pass        
 
     def k8s_helper(self, agent, params):
         if not os.environ['HOME']:
@@ -26,5 +39,3 @@ class Hedge:
         home = os.environ['HOME']    
         agent.ensureFile('/k8s/fwk8s.py', f"{home}/scripts/fwk8s.py")
         
-    def lazyvim(self, agent, params):
-        pass
